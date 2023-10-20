1d7625ed1244430bf21d92b97bba91ce03ba41ed
Greed7717 <greed170203@gmail.com>
2023-10-20 18:17:08 +0600

    'hw3'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
+
+
+
+def postListView(request):
+    post_value = models.Post.objects.all()
+    return render(request, 'post/post.html', {'post_key': post_value})
+
+
+
+
+
+
+
+
+
+
+
 
 def helloView(request):
     return HttpResponse("<h1>Hello! Its my project</h1>")
@@ -7,3 +25,5 @@ def now_dateView(request):
     return HttpResponse("<h1>17.10.23</h1>")
 def goodbye(request):
     return HttpResponse("<h1>Goodbye user!</h1>")
+
+
